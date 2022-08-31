909819e9e7d154ce678a1df4cc8de44c7f3848fe
João Pedro Nimuendaju <joao.nimuendaju@souunit.com.br>
2022-08-31 17:47:10 -0300

    teste com lista

--- Main.java
+++ Main.java
@@ -1,8 +1,28 @@
+import java.util.ArrayList;
+
 class Main {
   // tes
   public static void main(String[] args) {
     teste a = new teste("aasd");
     a.setName("kkkk");
+
+    String[] array = new String[10];
+    System.out.println(array.length);
+    System.out.println(array[0]);
+
+    ArrayList<Integer> lista = new ArrayList<>();
+    lista.add(1);
+    lista.add(2);
+    lista.add(3);
+
+    for (int i = 0; i < lista.size(); i++)  {
+      if(i > 1){
+        lista.remove(i);
+      }
+    }
+
+    System.out.println(lista);
+
     System.out.println(a.getName());
   }
 }
