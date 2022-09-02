ae372cea34e69df27dec7f6eba0b5a3086e24e06
João Pedro Nimuendaju <joao.nimuendaju@souunit.com.br>
2022-09-02 12:00:32 -0300

    coisas

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 
+import arvoreGenerica.GenericNode;
+
 class Main {
   // tes
   public static void main(String[] args) {
@@ -15,14 +17,24 @@ class Main {
     lista.add(2);
     lista.add(3);
 
-    for (int i = 0; i < lista.size(); i++)  {
-      if(i > 1){
-        lista.remove(i);
-      }
-    }
+    // for (int i = 0; i < lista.size(); i++)  {
+    //   if(i > 1){
+    //     lista.remove(i);
+    //   }
+    // }
+    ArrayList<teste> listaTeste = new ArrayList<>();
+    listaTeste.add(a);
+    listaTeste.remove(0);
 
-    System.out.println(lista);
+    
+    // lista.set(0, null);
 
-    System.out.println(a.getName());
+    // System.out.println(lista.size());
+
+    // System.out.println(a.getName());
+    
+    System.out.println(listaTeste);
+    
+
   }
 }
